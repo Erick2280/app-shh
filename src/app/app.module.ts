ce822aff6f64d7a59515c0058c902ec4342c521a
Erick Almeida <erickingles@gmail.com>
2019-05-26 02:52:10 -0300

    Plugins ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { IonicStorageModule } from '@ionic/storage';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,13 +19,25 @@ import { BackgroundMode } from '@ionic-native/background-mode/ngx';
 import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAdzbG_Hjl3hIEd8Dvgt-kIP9ZO6KgpTCg",
+  authDomain: "project-alana.firebaseapp.com",
+  databaseURL: "https://project-alana.firebaseio.com",
+  projectId: "project-alana",
+  storageBucket: "project-alana.appspot.com",
+  messagingSenderId: "2691348691",
+  appId: "1:2691348691:web:01e5a9067fed72db"
+}
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), IonicStorageModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), IonicStorageModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule],
   providers: [
     StatusBar,
+    GooglePlus,
     SplashScreen,
     InAppBrowser,
     Vibration,
